727cd231561d08b9cad75e47c1d1f04c92a5ef50
Layal <layalelzein2@gmail.com>
2024-02-07 16:46:57 +0100

    getters et setters

--- StringTreeSet.java
+++ StringTreeSet.java
@@ -4,12 +4,36 @@ public class StringTreeSet {
 
     public class Noeud {
 
-        String valeur;
+        String data;
         Noeud gauche;
-        Noeud droit;
+        Noeud droite;
+        Noeud suivant;
 
-        
-
+        public Noeud(String data, Noeud suivant, Noeud gauche, Noeud droite) {
+            this.data=data;
+            this.suivant=suivant;
+            this.gauche=gauche;
+            this.droite=droite;
+        }
+    
+        public void setSuivantDroite(Noeud n) {
+            this.suivant = n;
+        }
+    
+        public Noeud getSuivantGauche() {
+            return this.suivant;
+        }
+    
+        public void setData(String s) {
+            this.data = s;
+        }
+    
+        public String getData() {
+            return this.data;
+        }
 
     }
+
+
+
 }
